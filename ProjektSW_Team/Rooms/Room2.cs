5673236bce9fea526c6aec268ab02b8b0d7904e5
shew4uk <shew4uksuport@gmail.com>
2024-09-21 13:53:00 +0200

    Room 3.1

--- ProjektSW_Team/Rooms/Room2.cs
+++ ProjektSW_Team/Rooms/Room2.cs
@@ -14,13 +14,17 @@ namespace ProjektSW_Team.Rooms
         {
             doors.Add(new Door() { Position = new Point(8, 0), Size = new Size(4, 1), Room = typeof(Room1), PlayerX = 9 , PlayerY = 18});
             doors.Add(new Door() { Position = new Point(8, 19), Size = new Size(4, 1), Room = typeof(Room3), PlayerX = 9, PlayerY = 1 });
+            doors.Add(new Door() { Position = new Point(40, 13), Size = new Size(2, 3), Room = typeof(Room3_1), PlayerX = 2, PlayerY = 5 });
         }
         public override void DrawRoom(Canvas Rooms)
         {
-            Rooms.CanvasSize = new Size(20, 20);
+            Rooms.CanvasSize = new Size(42, 40);
             Rooms.Fill(Color.DarkGray);
+
+            Rooms.FillRect(20, 0, 40, 9, Color.Black);
+            Rooms.FillRect(0, 20, 42, 9, Color.Black);
             Rooms.FillRect(1, 1, 18, 18, Color.GreenYellow);
-            Rooms.FillRect(8, 19, 4, 1, Color.Blue);
+            Rooms.FillRect(1, 10, 39, 9, Color.GreenYellow);
         }
     }
 }
---
+++ ProjektSW_Team/Rooms/Room3_1.cs
@@ -0,0 +1,25 @@
+﻿using FastConsole.Engine.Elements;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSW_Team.Rooms
+{
+    internal class Room3_1 : Room
+    {
+        public Room3_1() : base()
+        {
+            doors.Add(new Door() { Position = new Point(0, 4), Size = new Size(2, 3), Room = typeof(Room2), PlayerX = 38, PlayerY = 14 });
+
+        }
+        public override void DrawRoom(Canvas Rooms)
+        {
+            Rooms.CanvasSize = new Size(43, 11);
+            Rooms.Fill(Color.DarkGray);
+            Rooms.FillRect(2, 1, 39, 9, Color.Gray);
+        }
+    }
+}
