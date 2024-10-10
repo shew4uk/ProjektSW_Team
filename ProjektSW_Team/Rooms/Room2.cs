cc4ce19fccc9d79c121aa6ed32798e3820056cc1
shew4uk <shew4uksuport@gmail.com>
2024-10-10 18:30:28 +0200

    Changed

--- ProjektSW_Team/Rooms/Room2.cs
+++ ProjektSW_Team/Rooms/Room2.cs
@@ -12,9 +12,19 @@ namespace ProjektSW_Team.Rooms
     {
         public Room2() : base()
         {
+            Objects.Add(new Obstacle(new Size(8, 1), Color.DarkGray) { Position = new Point(0, 0) });
+            Objects.Add(new Obstacle(new Size(2, 20), Color.DarkGray) { Position = new Point(0, 0) });
+            Objects.Add(new Obstacle(new Size(8, 1), Color.DarkGray) { Position = new Point(12, 0) });
+            Objects.Add(new Obstacle(new Size(2, 10), Color.DarkGray) { Position = new Point(19, 0) });
+            Objects.Add(new Obstacle(new Size(20, 1), Color.DarkGray) { Position = new Point(20, 9) });
+            Objects.Add(new Obstacle(new Size(2, 4), Color.DarkGray) { Position = new Point(40, 9) });
+            Objects.Add(new Obstacle(new Size(2, 4), Color.DarkGray) { Position = new Point(40, 16) });
+            Objects.Add(new Obstacle(new Size(25, 1), Color.DarkGray) { Position = new Point(12, 19) });
+            Objects.Add(new Obstacle(new Size(8, 1), Color.DarkGray) { Position = new Point(0, 19) });
             Objects.Add(new Door() { Position = new Point(8, 0), Size = new Size(4, 1), Room = typeof(Room1), PlayerX = 9 , PlayerY = 18});
             Objects.Add(new Door() { Position = new Point(8, 19), Size = new Size(4, 1), Room = typeof(Room3), PlayerX = 29, PlayerY = 1 });
             Objects.Add(new Door() { Position = new Point(40, 13), Size = new Size(2, 3), Room = typeof(Room3_1), PlayerX = 2, PlayerY = 5 });
+            
         }
         public override void DrawRoom(Canvas Rooms)
         {
--- ProjektSW_Team/Rooms/Room3.cs
+++ ProjektSW_Team/Rooms/Room3.cs
@@ -12,6 +12,16 @@ namespace ProjektSW_Team.Rooms
     {
         public Room3() : base()
         {
+            Objects.Add(new Obstacle(new Size(10, 1), Color.DarkGray) { Position = new Point(18, 0) });
+            Objects.Add(new Obstacle(new Size(10, 1), Color.DarkGray) { Position = new Point(32, 0) });
+            Objects.Add(new Obstacle(new Size(29, 1), Color.DarkGray) { Position = new Point(12, 19) });
+            Objects.Add(new Obstacle(new Size(2, 4), Color.DarkGray) { Position = new Point(40, 0) });
+            Objects.Add(new Obstacle(new Size(2, 13), Color.DarkGray) { Position = new Point(40, 7) });
+            Objects.Add(new Obstacle(new Size(2, 10), Color.DarkGray) { Position = new Point(0, 9) });
+            Objects.Add(new Obstacle(new Size(19, 1), Color.DarkGray) { Position = new Point(0, 9) });
+            Objects.Add(new Obstacle(new Size(2, 10), Color.DarkGray) { Position = new Point(18, 0) });
+
+            Objects.Add(new Obstacle(new Size(8, 1), Color.DarkGray){ Position = new Point(0, 19) });
             Objects.Add(new Door() { Position = new Point(28, 0), Size = new Size(4, 1), Room = typeof(Room2), PlayerX = 9, PlayerY = 18 });
             Objects.Add(new Door() { Position = new Point(8, 19), Size = new Size(4, 1), Room = typeof(Room4), PlayerX = 9, PlayerY = 1 });
             Objects.Add(new Door() { Position = new Point(40, 4), Size = new Size(2, 3), Room = typeof(Room3_2), PlayerX = 2, PlayerY = 5 });
@@ -20,7 +30,7 @@ namespace ProjektSW_Team.Rooms
         public override void DrawRoom(Canvas Rooms)
         {
             Rooms.CanvasSize = new Size(42, 40);
-            Rooms.Fill(Color.DarkGray);
+            Rooms.Fill(Color.Red);
 
             Rooms.FillRect(0, 0, 18, 9, Color.Black);
             Rooms.FillRect(0, 20, 42, 9, Color.Black);
