cb4610b388707a991ab83aeb03639017dae9abca
TomatoZwZ <alexboss0983@gmail.com>
2024-10-15 06:49:15 -0500

    boss

---
+++ ProjektSW_Team/Enemies/BlobBoss.cs
@@ -0,0 +1,59 @@
+﻿using FastConsole.Engine.Core;
+using FastConsole.Engine.Elements;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSW_Team.Enemies
+{
+    internal class BlobBoss : Element, IObject
+    {
+        public int Enemy_Hp;
+        public int Enemy_Damage;
+        private Canvas _canvas;
+        public BlobBoss()
+        {
+            Enemy_Hp = 500;
+            Enemy_Damage = 10;
+            _canvas = new Canvas(new Size(5, 5));
+            _canvas.Fill(Color.SaddleBrown);
+        }
+
+        public bool CanWalk { get; set; } = false;
+        public double LastHit { get; private set; }
+        public double LastMove { get; private set; }
+
+        public void Action()
+        {
+
+            if (Time.NowSeconds - LastHit > 0.8)
+            {
+                LastHit = Time.NowSeconds;
+                TheWorld.Instance.Player.TakeDamage(5);
+            }
+        }
+
+        public override void Update()
+        {
+
+            _canvas.Position = Position;
+
+            _canvas.Update();
+
+            if (Time.NowSeconds - LastMove > 5)
+            {
+                LastMove = Time.NowSeconds;
+                Player player = TheWorld.Instance.Player;
+                TheWorld.Instance.CurrentRoom.Objects.Add(new Cocoon { Position = new Point(16, 15), Size = new Size(1, 1) });
+
+            }
+        }
+        protected override void OnRender()
+        {
+            _canvas.Render();
+        }
+    }
+}
--- ProjektSW_Team/Rooms/BossRoom.cs
+++ ProjektSW_Team/Rooms/BossRoom.cs
@@ -13,15 +13,15 @@ namespace ProjektSW_Team.Rooms
         public BossRoom() : base()
         {
             Objects.Add(new Door() { Position = new Point(8, 0), Size = new Size(4, 1), Room = typeof(Room4), PlayerX = 9, PlayerY = 18 });
-            Objects.Add(new Enemy_Class { Position = new Point(10, 20),Size = new Size(1,1) });
-            Objects.Add(new Cocoon { Position = new Point(10, 20), Size = new Size(1, 1) });
-
+            //Objects.Add(new Enemy_Class { Position = new Point(10, 20),Size = new Size(1,1) });
+            Objects.Add(new Cocoon { Position = new Point(8, 15), Size = new Size(1, 1) });
+            Objects.Add(new BlobBoss { Position = new Point(10, 20), Size = Size });
         }
         public override void DrawRoom(Canvas Rooms)
         {
-            Rooms.CanvasSize = new Size(30, 30);
+            Rooms.CanvasSize = new Size(50, 30);
             Rooms.Fill(Color.DarkGray);
-            Rooms.FillRect(1, 1, 28, 28, Color.DarkOliveGreen);
+            Rooms.FillRect(1, 1, 48, 28, Color.DarkOliveGreen);
         }
 
     }
