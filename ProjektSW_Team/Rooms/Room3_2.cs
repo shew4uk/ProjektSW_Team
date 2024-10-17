d17090deaeb3c114791e0e063a039e0592e72674
shew4uk <shew4uksuport@gmail.com>
2024-10-17 13:22:01 +0200

    .

--- ProjektSW_Team/Rooms/Room3_2.cs
+++ ProjektSW_Team/Rooms/Room3_2.cs
@@ -15,7 +15,7 @@ namespace ProjektSW_Team.Rooms
         {
             Objects.Add(new Door() { Position = new Point(20, 4), Size = new Size(2, 3), Room = typeof(Room3_1), PlayerX = 2, PlayerY = 14 });
             Objects.Add(new Door() { Position = new Point(0, 4), Size = new Size(2, 3), Room = typeof(Room3), PlayerX = 38, PlayerY = 5 });
-<<<<<<< HEAD
+
             Objects.Add(new Enemy_Class { Position = new Point(19, 15), Size = new Size(1, 1) });
             Objects.Add(new Enemy_Class { Position = new Point(2, 15), Size = new Size(1, 1) });
             Objects.Add(new Trap() { Position = new Point(9, 4), Size = new Size(2, 1) });
@@ -24,7 +24,7 @@ namespace ProjektSW_Team.Rooms
             Objects.Add(new Trap() { Position = new Point(9, 6), Size = new Size(2, 1) });
             Objects.Add(new Trap() { Position = new Point(11, 5), Size = new Size(2, 1) });
             Objects.Add(new Trap() { Position = new Point(11, 6), Size = new Size(2, 1) });
-=======
+
             Objects.Add(new Enemy_Class { Position = new Point(12, 14), Size = new Size(1, 1) });
             Objects.Add(new Enemy_Class { Position = new Point(6, 14), Size = new Size(1, 1) });
             Objects.Add(new Trap() { Position = new Point(2, 1), Size = new Size(2, 1) });
@@ -35,7 +35,6 @@ namespace ProjektSW_Team.Rooms
             Objects.Add(new Obstacle(new Size(2, 1), Color.SaddleBrown) { Position = new Point(9, 7) });
             Objects.Add(new Obstacle(new Size(2, 1), Color.SaddleBrown) { Position = new Point(11, 6) });
             Objects.Add(new Obstacle(new Size(2, 1), Color.SaddleBrown) { Position = new Point(11, 7) });
->>>>>>> 0bc29b8d729eacc7af754a9f262ca64d1bb839b2
         }
         public override void DrawRoom(Canvas Rooms)
         {
